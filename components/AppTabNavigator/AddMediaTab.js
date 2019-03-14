080d6eac3e710cb83c15a5fd24fbee6b4b0fb0af
MikeJDev <ukmjanes@gmail.com>
2019-03-13 22:46:47 -0500

    added support for ios camera access and camera roll access

--- components/AppTabNavigator/AddMediaTab.js
+++ components/AppTabNavigator/AddMediaTab.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import Camera from 'react-native-camera';
 
 class AddMediaTab extends Component {
 
+  takePicture() {
+    this.camera
+      .capture()
+      .then((data) => { })
+      .catch(err => { });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>AddMediaTab</Text>
-      </View>
+      <Camera
+        ref={cam => { this.camera = cam }}
+        style={styles.preview}
+        aspect={Camera.constants.Aspect.fill}>
+        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+          [CAPTURE]
+        </Text>
+      </Camera>
     )
   }
 }
@@ -17,6 +30,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
   }
 })
 
