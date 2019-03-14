bc02019940b9929b557b23bee868a902edf032bb
MikeJDev <ukmjanes@gmail.com>
2019-03-14 16:05:30 -0500

    added user pic, buttons, and post numbers to profiletab

--- components/AppTabNavigator/ProfileTab.js
+++ components/AppTabNavigator/ProfileTab.js
@@ -3,6 +3,13 @@ import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import { Icon, Container, Left, Right, Body, Content, Button } from 'native-base'
 
 class ProfileTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0
+    }
+  }
+
   static navigationOptions = {
     headerLeft:
       <TouchableOpacity onPress={() => { }}>
@@ -10,6 +17,12 @@ class ProfileTab extends Component {
       </TouchableOpacity>,
     title: 'Mike Janes',
     headerRight: <Icon name="ios-menu" style={{ paddingRight: 10 }} />,
+  }
+
+  segment = (index) => {
+    this.setState({
+      activeIndex: index
+    })
   }
 
   render() {
@@ -25,14 +38,17 @@ class ProfileTab extends Component {
 
               <View style={{ flex: 3 }}>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+
                   <View style={{ alignItems: 'center' }}>
                     <Text>12</Text>
                     <Text style={{ fontSize: 10, color: 'grey' }}>Posts</Text>
                   </View>
+
                   <View style={{ alignItems: 'center' }}>
                     <Text>1043</Text>
                     <Text style={{ fontSize: 10, color: 'grey' }}>Followers</Text>
                   </View>
+
                   <View style={{ alignItems: 'center' }}>
                     <Text>3287</Text>
                     <Text style={{ fontSize: 10, color: 'grey' }}>Following</Text>
@@ -53,9 +69,47 @@ class ProfileTab extends Component {
               </View>
 
             </View>
+            <View style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
+              <Text style={{ fontWeight: 'bold' }}>Mike Janes</Text>
+              <Text>Software Engineer | Gym Bro | Dad</Text>
+              <Text>www.MikeJanes.com</Text>
+            </View>
           </View>
+
+          <View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', borderTopWidth: 1, borderTopColor: '#eae5e5' }}>
+              <Button transparent
+                onPress={() => { this.segment(0) }}
+                active={this.state.activeIndex === 0}
+              >
+                <Icon name='ios-apps' style={[this.state.activeIndex === 0 ? {} : { color: 'grey' }]} />
+              </Button>
+
+              <Button transparent
+                onPress={() => { this.segment(1) }}
+                active={this.state.activeIndex === 1}>
+                <Icon name='ios-list' style={[this.state.activeIndex === 1 ? {} : { color: 'grey' }]} />
+              </Button>
+
+              <Button transparent
+                onPress={() => { this.segment(2) }}
+                active={this.state.activeIndex === 2}>
+                <Icon name='ios-people' style={[this.state.activeIndex === 2 ? {} : { color: 'grey' }]} />
+              </Button>
+
+              <Button transparent
+                onPress={() => { this.segment(3) }}
+                active={this.state.activeIndex === 3}>
+                <Icon name='ios-bookmark' style={[this.state.activeIndex === 3 ? {} : { color: 'grey' }]} />
+              </Button>
+            </View>
+
+            {this.renderSection}
+
+          </View>
+
         </Content>
-      </Container>
+      </Container >
     )
   }
 }
